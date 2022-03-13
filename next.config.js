db1ae506d589958afdfbd181e8bd3da6f722eb5b
rbaks <andri.bakoson@gmail.com>
2022-03-14 00:10:41 +0300

    Modify next config and remove htaccess

--- next.config.js
+++ next.config.js
@@ -5,5 +5,5 @@ module.exports = {
    * Tell Next.js where the `public` folder is.
    * Replace `nextjs-github-pages` with your Github repo project name.
    */
-  assetPrefix: isProd ? "/test-rewrite/" : "",
+  assetPrefix: isProd ? "/rbaks.github.io/" : "",
 };
