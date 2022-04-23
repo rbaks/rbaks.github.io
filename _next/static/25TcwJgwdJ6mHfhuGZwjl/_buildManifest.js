52a17327e0eef27756662f1eb69f01864162f060
rbaks <rbaks@users.noreply.github.com>
2022-04-23 10:00:26 +0000

    deploy: 8327b1d6af5e5d279bb60756a179e56622e750f4

--- _next/static/25TcwJgwdJ6mHfhuGZwjl/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/955229c28454e75a.css","static/chunks/pages/index-3a2556e3abed4e3e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/doc":["static/chunks/pages/doc-77fc100a8cda9929.js"],sortedPages:["/","/_app","/_error","/doc"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/6uEIYCN6YuZF3yYS9QQC3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f121935b53d61b85.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/doc":["static/chunks/pages/doc-77fc100a8cda9929.js"],sortedPages:["/","/_app","/_error","/doc"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
